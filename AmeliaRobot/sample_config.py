48d395dd26e4d5f6e3cd6e75a4f588f16bf17f81
KazuyaNorthen <140612076+KazuyaNorthen@users.noreply.github.com>
2023-07-26 18:53:26 +0700

    sample_config.py

--- AmeliaRobot/sample_config.py
+++ AmeliaRobot/sample_config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Kittu"
-    SUPPORT_CHAT = "Kora_Support"  # Your own group for support, do not add the @
+    API_ID = 27776767  # integer value, dont use ""
+    API_HASH = "e7b0d8f7b037df9ff8b300816e90080b"
+    TOKEN = "6375362038:AAFYyElGnq2QsDSuOSSjiJVnsY8jaWfyPAM"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 6044333279  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "devilsweapon"
+    SUPPORT_CHAT = "pegasusfederation2"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001960654405
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001960654405
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
